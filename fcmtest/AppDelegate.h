5613becc656559d144784013988739988ce113ca
james <james.zhen@hyweb.com.tw>
2016-09-13 14:00:47 +0800

    Initial Commit

---
+++ fcmtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fcmtest
+//
+//  Created by t00javateam@gmail.com on 2016/9/13.
+//  Copyright © 2016年 t00javateam@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fcmtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fcmtest
+//
+//  Created by t00javateam@gmail.com on 2016/9/13.
+//  Copyright © 2016年 t00javateam@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
